ace75d9253c00e8eff1e0d0accbd03ebc15855e7
AshlingScott <93444556+AshlingScott@users.noreply.github.com>
2021-11-01 21:52:30 -0400

    Wakeup Functionality complete, working on Combo Frame

--- GGAPP.py
+++ GGAPP.py
@@ -2,25 +2,22 @@ from tkinter import*
 from PIL import ImageTk, Image
 from Character import Character
 
-#characters supported by app
-character_list = [
-	"Chipp",
-	"Ramlethal",
-	"JackO"
-]
-
-#create Characters
-Chipp = Character("Chipp", "ChippWakeups.png")
-Ram = Character("Ramlethal", "RamlethalWakeups.png")
-
 #create window
 root = Tk()
 root.title("Guilty Gear Companion App")
 root.geometry("850x750")
 root.resizable(False, False)
 
+#characters supported by app
+character_selected = StringVar()
+character_list = [
+	"Chipp",
+	"Ramlethal",
+	"JackO"
+]
+
 #GG logo
-gg_logo = ImageTk.PhotoImage(Image.open("GGStriveLogo.png"))
+gg_logo = ImageTk.PhotoImage(Image.open("Images/GGStriveLogo.png"))
 logo_label = Label(image=gg_logo)
 logo_label.grid(row=0, column=1, columnspan=2)
 
@@ -28,45 +25,76 @@ logo_label.grid(row=0, column=1, columnspan=2)
 main_frame = LabelFrame(root, padx=20, pady=20)
 main_frame.grid(row=1, column=0, columnspan=4)
 
-#character select widgets
-character_selected = StringVar()
-character_selected.set("Chipp")
+#switches to Wakeup Frame, defaults to Chipp as selected character
+def switch_to_wakeup_chart():
+	global drop_down
+	drop_down.destroy()
 
-#changes displayed image based on dropdown choice
-def update_char_selected(new_char):
-	global chart_label
+	character_selected.set("Chipp")
+	wakeup_chart = ImageTk.PhotoImage(Image.open("Images/ChippWakeups.png"))
+	chart_label = Label(main_frame, image=wakeup_chart)
+	chart_label.photo = wakeup_chart
+	chart_label.grid(row=2, column=0, columnspan=2)
 
+	character_label = Label(main_frame, text="Wakeup Options", font="Helvetica 18 bold")
+	drop_down = OptionMenu(main_frame, character_selected, *character_list, command=update_wakeup_chart)
+
+	character_label.grid(row=0, column=0)
+	drop_down.grid(row=0, column=1)
+
+#updates the Wakeup Frame to the selected character
+def update_wakeup_chart(new_char):
 	if new_char == "Chipp":
-		chart_label.destroy()
 		wakeup_chart = ImageTk.PhotoImage(Image.open("Images/ChippWakeups.png"))
-
 	if new_char == "Ramlethal":
-		chart_label.destroy()
 		wakeup_chart = ImageTk.PhotoImage(Image.open("Images/RamlethalWakeups.png"))
-
 	if new_char == "JackO":
-		chart_label.destroy()
 		wakeup_chart = ImageTk.PhotoImage(Image.open("Images/JackOWakeups.png"))
 
 	chart_label = Label(main_frame, image=wakeup_chart)
 	chart_label.photo = wakeup_chart
 	chart_label.grid(row=2, column=0, columnspan=2)
 
-#the wakeup chart, changes depending on character select
-wakeup_chart = ImageTk.PhotoImage(Image.open("Images/ChippWakeups.png"))
-chart_label = Label(main_frame, image=wakeup_chart)
-chart_label.photo = wakeup_chart
-chart_label.grid(row=2, column=0, columnspan=2)
+def switch_to_combo_chart():
+	global drop_down
+	drop_down.destroy()
+
+	character_selected.set("Chipp")
+	wakeup_chart = ImageTk.PhotoImage(Image.open("Images/ChippCombos.png"))
+	chart_label = Label(main_frame, image=wakeup_chart)
+	chart_label.photo = wakeup_chart
+	chart_label.grid(row=2, column=0, columnspan=2)
+
+	character_label = Label(main_frame, text="Combo Routes", font="Helvetica 18 bold")
+	drop_down = OptionMenu(main_frame, character_selected, *character_list, command=update_combo_chart)
+
+	character_label.grid(row=0, column=0)
+	drop_down.grid(row=0, column=1)
+
+def update_combo_chart():
+	if new_char == "Chipp":
+		combo_chart = ImageTk.PhotoImage(Image.open("Images/ChippCombos.png"))
+	if new_char == "Ramlethal":
+		combo_chart = ImageTk.PhotoImage(Image.open("Images/RamlethalWakeups.png"))
+	if new_char == "JackO":
+		combo_chart = ImageTk.PhotoImage(Image.open("Images/JackOWakeups.png"))
+
+	chart_label = Label(main_frame, image=combo_chart)
+	chart_label.photo = combo_chart
+	chart_label.grid(row=2, column=0, columnspan=2)
 
 character_label = Label(main_frame, text="Wakeup Options", font="Helvetica 18 bold")
-drop_down = OptionMenu(main_frame, character_selected, *character_list, command=update_char_selected)
+drop_down = OptionMenu(main_frame, character_selected, *character_list, command=update_wakeup_chart)
 
 character_label.grid(row=0, column=0)
 drop_down.grid(row=0, column=1)
 
+chart_label = Label(main_frame)
+switch_to_wakeup_chart()
+
 #Options on main page
-wakeup_options_button = Button(root, text="Wakeup Options", width=25)
-optimal_routes_button = Button(root, text="Optimal Combo Routes", width=25)
+wakeup_options_button = Button(root, text="Wakeup Options", width=25, command=switch_to_wakeup_chart)
+optimal_routes_button = Button(root, text="Optimal Combo Routes", width=25, command=switch_to_combo_chart)
 matchup_chart_button = Button(root, text="Matchup Chart", width=25)
 placeholder_button = Button(root, text="Placeholder", width=25)
 
